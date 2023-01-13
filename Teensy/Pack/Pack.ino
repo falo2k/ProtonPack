f6f07ec8881639f5763179dbc5c96bb3c6a4b0c4
Oli Wright <oli@floor4.co.uk>
2023-01-13 17:38:06 +0000

    Project setup for Teensy

---
+++ Teensy/Pack/Pack.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Pack.ino
+ Created:	1/13/2023 5:13:26 PM
+ Author:	oliwr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Teensy/Wand/Wand.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Wand.ino
+ Created:	1/13/2023 5:10:39 PM
+ Author:	oliwr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
